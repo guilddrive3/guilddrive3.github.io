aa70af54b91d21998330c6380dd464c554f4b398
KeithLevi <guilddrive@gmail.com>
2021-08-19 21:44:36 -0500

    added the d21 objects files

---
+++ d21objects/calculator.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = {calculator }; //add all of your function names here that you need for the node mocha tests
+
+const calculator = {};  // implement this
---
+++ d21objects/calculatorTests.js
@@ -0,0 +1,31 @@
+"use strict";
+/* global assert calculator */
+
+/* comment out the following for the browser */
+const assert = require("assert");  //always need this with node
+const myExports = require("./app.js");  //with node need the name of your file with your functions here
+const calculator = myExports.calculator;  //do this for all of the functions used in the Mocha tests
+
+
+
+
+describe("new calculator", function () {
+ 
+
+    it("checks initial values 2 and 3", function () {
+        assert.strictEqual(calculator.a, 2);
+        assert.strictEqual(calculator.b, 3);
+    });
+
+    it("when 2 and 3 are entered, the sum is 5", function () {
+        assert.strictEqual(calculator.sum(), 5);
+    });
+
+    it("when 2 and 3 are entered, the product is 6", function () {
+        assert.strictEqual(calculator.mul(), 6);
+    });
+
+
+});
+
+
