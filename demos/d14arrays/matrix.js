f59518df3fdd6c09e6c2477537beb6e78ae777c1
KeithLevi <guilddrive@gmail.com>
2021-09-01 21:05:47 -0500

    d14demo

---
+++ demos/d14arrays/matrix.js
@@ -0,0 +1,15 @@
+"use strict";
+/* eslint-disable */
+
+let matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]; 
+    
+    console.log(matrix);
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            console.log(matrix[i][j]);
+        }
+    }
+    
