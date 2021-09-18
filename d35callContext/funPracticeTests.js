93506e19829a17e2c5b02eb13f6e5de68847ad90
KeithLevi <guilddrive@gmail.com>
2021-09-18 18:17:40 -0500

    updates to d36callback assignment

--- d35callContext/funPracticeTests.js
+++ d35callContext/funPracticeTests.js
@@ -1,7 +1,7 @@
 "use strict";
 /* You need the assert and function name declarations to test in node.  
-Comment these out when you send it to the browser with the index.html mocha setup page.  */
-
+Comment these out when you send it to the browser with the index.html mocha setup page. 
+ */
 const assert = require("assert");  //always need this with node
 const myExports = require("./funTests.js");  //with node need the name of your file with your functions here
 const myMap = myExports.myMap;  //do this for all of the functions used in the Mocha tests
@@ -43,12 +43,12 @@ describe("map filter reduce", function () {
 
     it("tests reduce with sum", function () {
         function sum(acc, number) { return (number + acc);}
-        assert.strictEqual(myReduce([1, 2, 3], sum, 0), 7);
+        assert.strictEqual(myReduce([1, 2, 3, 4], sum, 0), 10);
     });
 
     it("tests reduce with mult", function () {
         function mult(acc, number) { return (number * acc);}
-        assert.strictEqual(myReduce([1, 2, 3], sum, 0), 6);
+        assert.strictEqual(myReduce([1, 2, 3, 4], mult, 1), 24);
     });
 
 });
---
+++ d35callContext/funTests.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser.
+*/
+module.exports = { myMap, myFilter, myReduce }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} func 
+ * @returns 
+ */
+function myMap(arr, func) {
+//IMPLEMENTATION NEEDED
+}
+
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} func 
+ * @returns 
+ */
+function myFilter(arr, func) {
+//IMPLEMENTATION NEEDED
+}
+
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} func 
+ * @param {*} initialValue 
+ * @returns 
+ */
+function myReduce(arr, func, initialValue) {
+    //IMPLEMENTATION NEEDED
+    return  ;
+    }
