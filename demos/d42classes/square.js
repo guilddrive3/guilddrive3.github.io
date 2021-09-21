acf44de8c8d486be5717f1580b4ce499b3b04b8e
KeithLevi <guilddrive@gmail.com>
2021-09-21 12:58:11 -0500

    Update square.js

--- demos/d42classes/square.js
+++ demos/d42classes/square.js
@@ -30,3 +30,28 @@ console.log("expect 1: ", Object.getOwnPropertyNames(constSquare1).length);
 const classSquare = new Square2(10);
 console.log("expect 100: ", classSquare.area());
 console.log("expect 1: ", Object.getOwnPropertyNames(classSquare).length);
+
+
+/* Create two morphableSquare objects and draw the object diagram for all the objects involved 
+square objects
+constructor function
+prototype objects
+links
+*/
+class MorphableSquare extends Square2{
+    constructor(size, color, className){
+        super(size);
+        this.color = color;
+        this.className = className;
+    }
+    /* has method morph that will set the value of the className property to the value of the color property */
+    morph(){
+        this.className = this.color;
+    }
+
+}
+const morph2blue = new MorphableSquare(3, "blue", "white");
+const morph2green = new MorphableSquare(3, "green", "white");
+console.log("expect 9: ", morph2blue.area());
+console.log("expect 3: ", Object.getOwnPropertyNames(morph2blue).length);
+
