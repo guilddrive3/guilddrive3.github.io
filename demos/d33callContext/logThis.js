8e448b96bed25ecebbae29a76cf28450f115bdd4
KeithLevi <guilddrive@gmail.com>
2022-03-11 21:40:05 -0600

    demos

--- demos/d33callContext/logThis.js
+++ demos/d33callContext/logThis.js
@@ -2,19 +2,21 @@
 /* eslint-disable */
 
 function a() {
-    console.log("this in function a: ", this);}
-  
-  const b = {
-    dog: 'fido',
-    log: function() {
-      console.log("this in method/function log: ", this);  }}
-  
-  console.log("this in console.log: ", this); 
-  a(); 
-  b.log(); 
-  let mylog = b.log;
-  mylog();
+  console.log("this in function a: ", this);
+}
+
+const b = {
+  dog: 'fido',
+  log: function () {
+    console.log("this in method/function log: ", this);
+  }
+}
+
+console.log("this in console.log: ", this);
+a();
+b.log();
+let mylog = b.log;
+mylog();
 
 
 
-  
---
+++ demos/d41classes/arrow.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * Illustrates that arrow function will work for method if it is used in the constructor function
+ * --since it is a (constructor) function it will have lexical environment
+ * --has to be in constructor, so is not on prototype chain (copied in every object instance)
+ */
+class User {
+    /**
+     * 
+     * @param {String} name is a person's name
+     */
+    constructor(name) {
+        this.name = name;
+        this.sayHello = () => console.log("Hello and good day from me, ", this.name);
+        //lexical scope is the constructor "function", so this method is an inner function and arrow works in an inner "function"
+        //i.e., functions have lexical environments, in contrast to objects
+    }
+    /**
+     * @returns {undefined} 
+     */
+    sayHi() { console.log("Hi, my name is ", this.name); }
+}
+
+let user1 = new User("Fred");
+user1.sayHi();
+user1.sayHello();
+
+/**
+ * my class component
+ */
+class Component {
+    /**
+     * 
+     */
+    constructor() {
+        this.count = 0;
+        this.increment= () => { this.count++ }
+    }
+
+}
