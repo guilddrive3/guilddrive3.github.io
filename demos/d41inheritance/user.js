71625d2e47ffe46aa7c7631bb1c7db7f297c2d73
KeithLevi <guilddrive@gmail.com>
2022-03-12 16:03:51 -0600

    user

--- demos/d41inheritance/user.js
+++ demos/d41inheritance/user.js
@@ -8,15 +8,18 @@ function User(firstname, lastname, birthDate) {
         this.birthDate = birthDate;
     }
 
-    User.prototype = {
-        getFullName: getFullName,
-        getAge: getAge
-    }
+    // User.prototype = {
+    //     getFullName: getFullName,
+    //     getAge: getAge
+    // }
+    User.prototype.getFullName = getFullName;
+    User.prototype.getAge = getAge;
 
     let user1 = new User('John', 'Smith', new Date('2000-10-01'));
     let user2 = new User('Edward', 'Hopkins', new Date('1991-11-14'));
     function getFullName() { return this.firstname + ' ' + this.lastname;}
-    function getAge() {return new Date().getFullYear() - this.birthDate.getFullYear();}
+    
+    function getAge() {return new Date().getFullYear() - this.birthDate.getFullYear();}
     
 
     
