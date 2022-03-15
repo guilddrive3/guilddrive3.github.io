26e9e4d24c1c7cdb49266f5d70ca722d20f16c8a
KeithLevi <guilddrive@gmail.com>
2022-03-15 14:49:52 -0500

    Update bankTests.js

--- d41class/bankTests.js
+++ d41class/bankTests.js
@@ -131,113 +131,113 @@ describe("SavingsAccount", () => {
 
 });
 
-// describe("CheckingAccount", () => {
-//     let checking = undefined;
-//     describe("constructor(number, overdraft)", () => {
-//         it("takes a number and the overdraft limit and makes a checking account", () => {
-//             checking = new CheckingAccount(1, 200);
-//             assert.equal(checking.getNumber(), 1);
-//             assert.equal(checking.getOverdraft(), 200);
-//             assert.equal(checking.constructor, CheckingAccount);
-//         });
-//     });
-//     describe("overdraft getter / setter", () => {
-//         it("can get the overdraft limit for this account", () => {
-//             assert.equal(checking.getOverdraft(), 200);
-//         });
-//         it("can set the overdraft limit for this account", () => {
-//             checking.setOverdraft(250);
-//             assert.equal(checking.getOverdraft(), 250);
-//         });
-//     });
-//     describe("withdraw(amount) method", () => {
-//         it("can withdraw into negative up to the overdraft limit", () => {
-//             checking.withdraw(200);
-//             assert.equal(checking.getBalance(), -200);
-//         });
-//         it("throws an error if you go beyond the limit", () => {
-//             let makeError = () => {
-//                 checking.withdraw(100);
-//             };
-//             assert.throws(makeError, Error, "Insufficient funds, cannot withdraw beyond overdraft limit");
-//         });
-//         it("throws a RangeError if you give a number <= 0", () => {
-//             let makeError = () => {
-//                 checking.deposit(-1);
-//             };
-//             assert.throws(makeError, RangeError, "Deposit amount has to be greater than zero");
-//         });
-//     });
-//     describe("toString() method", () => {
-//         it("returns a string representation of the SavingsAccount", () => {
-//             assert.equal("CheckingAccount 1: balance: -200 overdraft limit: 250", checking.toString());
-//         });
-//     });
-//     describe("endOfMonth() method", ()=>{
-//         it("returns a warning string when the balance is below zero", ()=>{
-//             assert.equal(checking.endOfMonth(), "Warning, low balance CheckingAccount 1: balance: -200 overdraft limit: 250");
-//         });
-//         it("returns an empty string when balance above zero", ()=>{
-//             checking.deposit(1000);
-//             assert.equal(checking.endOfMonth(), "");
-//         });
-//     });
+describe("CheckingAccount", () => {
+    let checking = undefined;
+    describe("constructor(number, overdraft)", () => {
+        it("takes a number and the overdraft limit and makes a checking account", () => {
+            checking = new CheckingAccount(1, 200);
+            assert.equal(checking.getNumber(), 1);
+            assert.equal(checking.getOverdraft(), 200);
+            assert.equal(checking.constructor, CheckingAccount);
+        });
+    });
+    describe("overdraft getter / setter", () => {
+        it("can get the overdraft limit for this account", () => {
+            assert.equal(checking.getOverdraft(), 200);
+        });
+        it("can set the overdraft limit for this account", () => {
+            checking.setOverdraft(250);
+            assert.equal(checking.getOverdraft(), 250);
+        });
+    });
+    describe("withdraw(amount) method", () => {
+        it("can withdraw into negative up to the overdraft limit", () => {
+            checking.withdraw(200);
+            assert.equal(checking.getBalance(), -200);
+        });
+        it("throws an error if you go beyond the limit", () => {
+            let makeError = () => {
+                checking.withdraw(100);
+            };
+            assert.throws(makeError, Error, "Insufficient funds, cannot withdraw beyond overdraft limit");
+        });
+        it("throws a RangeError if you give a number <= 0", () => {
+            let makeError = () => {
+                checking.deposit(-1);
+            };
+            assert.throws(makeError, RangeError, "Deposit amount has to be greater than zero");
+        });
+    });
+    describe("toString() method", () => {
+        it("returns a string representation of the SavingsAccount", () => {
+            assert.equal("CheckingAccount 1: balance: -200 overdraft limit: 250", checking.toString());
+        });
+    });
+    describe("endOfMonth() method", ()=>{
+        it("returns a warning string when the balance is below zero", ()=>{
+            assert.equal(checking.endOfMonth(), "Warning, low balance CheckingAccount 1: balance: -200 overdraft limit: 250");
+        });
+        it("returns an empty string when balance above zero", ()=>{
+            checking.deposit(1000);
+            assert.equal(checking.endOfMonth(), "");
+        });
+    });
 
-// });
+});
 
 
-// describe("Bank Class", () => {
-//     let bank = undefined;
-//     describe("constructor()", () => {
-//         it("creates a Bank object", () => {
-//             bank = new Bank();
-//             assert.equal(bank.constructor, Bank);
-//             assert.equal(bank._accounts.length, 0);
-//         });
-//     });
-//     describe("addAccount()", () => {
-//         it("adds an Account object to it array", () => {
-//             bank.addAccount();
-//             assert.equal(bank._accounts[0].getNumber(), 1);
-//             assert.equal(bank._accounts[0].getBalance(), 0);
-//         });
-//     });
-//     describe("addSavingsAccount(interest)", () => {
-//         it("adds an SavingsAccount object to it array", () => {
-//             bank.addSavingsAccount(2.5);
-//             assert.equal(bank._accounts[1].getNumber(), 2);
-//             assert.equal(bank._accounts[1].getBalance(), 0);
-//             assert.equal(bank._accounts[1].getInterest(), 2.5);
-//         });
-//     });
-//     describe("addCheckingAccount(overdraft)", () => {
-//         it("adds an CheckingAccount object to it array", () => {
-//             bank.addCheckingAccount(500);
-//             assert.equal(bank._accounts[2].getNumber(), 3);
-//             assert.equal(bank._accounts[2].getBalance(), 0);
-//             assert.equal(bank._accounts[2].getOverdraft(), 500);
-//         });
-//     });
-//     describe("closeAccount(number)", () => {
-//         it("closes the account with the given number", () => {
-//             bank.closeAccount(1);
-//             assert.equal(bank._accounts.length, 2);
-//             assert.equal(bank._accounts[1].getNumber(), 3);
-//             assert.equal(bank._accounts[1].getBalance(), 0);
-//             assert.equal(bank._accounts[1].getOverdraft(), 500);
-//         });
-//     });
-//     describe("accountReport()", () => {
-//         it("returns a string describing all accounts", () => {
-//             assert.equal(bank.accountReport(), "SavingsAccount 2: balance: 0 interest: 2.5\nCheckingAccount 3: balance: 0 overdraft limit: 500\n");
-//         });
-//     });
-//     describe("endOfMonth()", ()=>{
-//         it("returns a string with actions related to the accounts", ()=>{
-//             bank._accounts[0].deposit(100);
-//             bank._accounts[1].withdraw(100);
-//             bank.addAccount();
-//             assert.equal(bank.endOfMonth(), "Interest added SavingsAccount 2: balance: 102.5 interest: 2.5\nWarning, low balance CheckingAccount 3: balance: -100 overdraft limit: 500\n");
-//         });
-//     });
-// });
+describe("Bank Class", () => {
+    let bank = undefined;
+    describe("constructor()", () => {
+        it("creates a Bank object", () => {
+            bank = new Bank();
+            assert.equal(bank.constructor, Bank);
+            assert.equal(bank._accounts.length, 0);
+        });
+    });
+    describe("addAccount()", () => {
+        it("adds an Account object to it array", () => {
+            bank.addAccount();
+            assert.equal(bank._accounts[0].getNumber(), 1);
+            assert.equal(bank._accounts[0].getBalance(), 0);
+        });
+    });
+    describe("addSavingsAccount(interest)", () => {
+        it("adds an SavingsAccount object to it array", () => {
+            bank.addSavingsAccount(2.5);
+            assert.equal(bank._accounts[1].getNumber(), 2);
+            assert.equal(bank._accounts[1].getBalance(), 0);
+            assert.equal(bank._accounts[1].getInterest(), 2.5);
+        });
+    });
+    describe("addCheckingAccount(overdraft)", () => {
+        it("adds an CheckingAccount object to it array", () => {
+            bank.addCheckingAccount(500);
+            assert.equal(bank._accounts[2].getNumber(), 3);
+            assert.equal(bank._accounts[2].getBalance(), 0);
+            assert.equal(bank._accounts[2].getOverdraft(), 500);
+        });
+    });
+    describe("closeAccount(number)", () => {
+        it("closes the account with the given number", () => {
+            bank.closeAccount(1);
+            assert.equal(bank._accounts.length, 2);
+            assert.equal(bank._accounts[1].getNumber(), 3);
+            assert.equal(bank._accounts[1].getBalance(), 0);
+            assert.equal(bank._accounts[1].getOverdraft(), 500);
+        });
+    });
+    describe("accountReport()", () => {
+        it("returns a string describing all accounts", () => {
+            assert.equal(bank.accountReport(), "SavingsAccount 2: balance: 0 interest: 2.5\nCheckingAccount 3: balance: 0 overdraft limit: 500\n");
+        });
+    });
+    describe("endOfMonth()", ()=>{
+        it("returns a string with actions related to the accounts", ()=>{
+            bank._accounts[0].deposit(100);
+            bank._accounts[1].withdraw(100);
+            bank.addAccount();
+            assert.equal(bank.endOfMonth(), "Interest added SavingsAccount 2: balance: 102.5 interest: 2.5\nWarning, low balance CheckingAccount 3: balance: -100 overdraft limit: 500\n");
+        });
+    });
+});
