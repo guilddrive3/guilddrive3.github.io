2f8ea741995fb2d7edadb245c321d02717e01892
KeithLevi <guilddrive@gmail.com>
2022-09-02 20:55:14 -0500

    cleaning up old solution files

--- demos/d11iteration/logical_operators.js
+++ demos/d11iteration/logical_operators.js
@@ -3,6 +3,6 @@
 let x = 10;
 let y = 5;
 
-console.log(x>=10 && y<10) //true
-console.log(x===10 || y===3) //true
-console.log(x<99 && y>10) //false
+console.log(x>=10 && y<10); //true
+console.log(x===10 || y===3); //true
+console.log(x<99 && y>10); //false
