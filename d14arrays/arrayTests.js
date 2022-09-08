20991917daf6372202ccca4c1559099cea30086c
KeithLevi <guilddrive@gmail.com>
2022-09-08 14:31:27 -0500

    ss

--- d14arrays/arrayTests.js
+++ d14arrays/arrayTests.js
@@ -2,12 +2,12 @@
 
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.
-*/
+
 const assert = require("assert");  //always need this with node
 const myExports = require("./arrays.js");  //with node need the name of your file with your functions here
 const maxOfThree = myExports.maxOfThree;  //do this for all of the functions used in the Mocha tests
 const multiply = myExports.multiply;
-const sum = myExports.sum;
+const sum = myExports.sum;*/
 
 
 /* global assert maxOfThree sum multiply findLongestWord reverseArray reverseArrayInPlace scoreExams generateArray */
--- d14arrays/arrays.js
+++ d14arrays/arrays.js
@@ -1,9 +1,9 @@
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
-*/ 
-module.exports = {maxOfThree: maxOfThree, sum: sum, multiply: multiply }; //add all of your function names here that you need for the node mocha tests
 
+module.exports = {maxOfThree: maxOfThree, sum: sum, multiply: multiply }; //add all of your function names here that you need for the node mocha tests
+*/ 
 /**
  * 
  * @param {number} a is a number
