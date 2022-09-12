4906c6a303f4a5d2e3fb6458a30fa61b53afb7c2
KeithLevi <guilddrive@gmail.com>
2022-09-12 12:32:38 -0500

    Create printNumbers.js

---
+++ demos/d21objects/printNumbers.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const numbers = {
+    one: 1,
+    two: 22,
+    three: 333,
+    four: 444
+}
+
+const properties = ["one", "two", "three", "four"];
+
+//write a for loop to print out the values of the properties in numbers.  You must use the properties array to access the values.  You cannot directly write console.log(numbers.one)  …
+for (const prop of properties){
+    console.log(numbers[prop]);
+}
+
