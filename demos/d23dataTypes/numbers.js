b483a3da3aa1d6edfe82e5dc0d187f8348e6e09e
KeithLevi <guilddrive@gmail.com>
2022-09-14 11:58:49 -0500

    class demos

---
+++ demos/d23dataTypes/numbers.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const numbers = [1, 5, 18, 2, 77, 108];
+/* use filter, find, and findIndex to find
+ all the even numbers
+ the first even number
+ the index of the first even number
+*/
+
+/**
+ * 
+ * @param {*} num is any integer
+ * @returns {boolean} true if even else false
+ */
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const evens = numbers.filter(isEven);
+console.log("expect [18,2,108: " , evens);
+
+console.log("expect 18: ", numbers.find(isEven));
+console.log("expect 2: ", numbers.findIndex(isEven));
+
+
