5919f79449ce8a14c8b9e1f443bfd6891cfda0e1
KeithLevi <guilddrive@gmail.com>
2022-09-14 12:29:11 -0500

    Update foreach.js

--- demos/d23dataTypes/foreach.js
+++ demos/d23dataTypes/foreach.js
@@ -3,7 +3,7 @@
 /*  use forEach to log all the even elements of an array to the console */
 
 
-//[1, 5, 16, 3, 108].forEach(element => { if (element % 2 === 0) console.log(element); });
+[1, 5, 16, 3, 108].forEach(element => { if (element % 2 === 0) console.log(element); });
 
 [1, 5, 16, 3, 108].forEach(logEvens);
 
