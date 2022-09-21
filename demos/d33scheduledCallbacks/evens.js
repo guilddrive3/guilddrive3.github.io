095a759fd12d58366cb97dd9da698561c81533da
KeithLevi <guilddrive@gmail.com>
2022-09-21 11:57:40 -0500

    Update evens.js

--- demos/d33scheduledCallbacks/evens.js
+++ demos/d33scheduledCallbacks/evens.js
@@ -1,9 +1,29 @@
 "use strict";
 /* 
-write a function, multiplyEvens, that can be called with any number of arguments and returns the product of the even arguments
+write a function, multiplyEvens, that can be called with any number of arguments and 
+returns the product of the even arguments
 do first using the arguments object
 then using …rest parameter
 */
 
 
 console.log("expect 48: ", multiplyEvens(1, 6, 3, 4, 17, 2));
+
+/**
+ * 
+ * @returns {number} product of arguments that are even numbers 
+ */
+function multiplyEvens(...argsArray){
+    let evensProduct = 1;
+    // for (const num of arguments){
+    //     if (num % 2 === 0){
+    //         evensProduct = evensProduct * num;
+    //     }
+    for (const num of argsArray){
+        if (num % 2 === 0){
+            evensProduct = evensProduct * num;
+        }
+    }
+
+    return evensProduct;
+}
