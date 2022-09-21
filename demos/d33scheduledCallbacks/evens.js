9eeb1910d0f57e0c9858a0fa089ae2a2e3b87425
KeithLevi <guilddrive@gmail.com>
2022-09-20 21:26:23 -0500

    demo updates

---
+++ demos/d33scheduledCallbacks/evens.js
@@ -0,0 +1,9 @@
+"use strict";
+/* 
+write a function, multiplyEvens, that can be called with any number of arguments and returns the product of the even arguments
+do first using the arguments object
+then using …rest parameter
+*/
+
+
+console.log("expect 48: ", multiplyEvens(1, 6, 3, 4, 17, 2));
