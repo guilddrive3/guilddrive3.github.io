93cd3570a12b744f178eb39b9b71a4a8d36d19ce
KeithLevi <guilddrive@gmail.com>
2022-09-24 12:49:52 -0500

    Update user.js

--- demos/d36inheritance/user.js
+++ demos/d36inheritance/user.js
@@ -13,12 +13,19 @@ function User(firstname, lastname, birthDate) {
     this.birthDate = birthDate;
 }
 
-
+const userPrototype = {
+    getFullName: getFullName,
+    getAge: getAge
+}
+User.prototype = userPrototype;
 
 let user1 = new User('John', 'Smith', new Date('2000-02-01'));
 let user2 = new User('Edward', 'Hopkins', new Date('1991-11-14'));
 function getFullName() { return this.firstname + ' ' + this.lastname; }
 function getAge() { return new Date().getFullYear() - this.birthDate.getFullYear(); }
+
+// User.prototype.getFullName = getFullName;
+// User.prototype.getAge = getAge;
 
 
 
