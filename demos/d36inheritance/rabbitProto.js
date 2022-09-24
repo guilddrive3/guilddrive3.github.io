677bdd9a7eb06ddc90f973af22d7fde2585cfbfa
KeithLevi <guilddrive@gmail.com>
2022-09-24 11:53:19 -0500

    Update rabbitProto.js

--- demos/d36inheritance/rabbitProto.js
+++ demos/d36inheritance/rabbitProto.js
@@ -7,15 +7,17 @@ let animal = { eats: true };
  * @param {string} name of rabbit
  * @returns {Object} constructor
  */
-//function Rabbit(name) {  this.name = name;}
+// function Rabbit(name) {  this.name = name;}
+// // 
+// Rabbit.prototype = animal;
 
-//Rabbit.prototype = animal;
-
-let rabbit = {
+let rabbit = 
+{
     name: "White Rabbit",
-    __proto__: animal
+   // __proto__: animal
 };
-//new Rabbit("White Rabbit");
+rabbit.__proto__ = animal;
+// new Rabbit("White Rabbit");
 
 console.log(rabbit.eats); // true
 
