ba68b0b79ce2c8b3da4a3c732fd6f24195a40ec6
KeithLevi <guilddrive@gmail.com>
2022-09-26 14:45:05 -0500

    Update square.js

--- demos/d41classes/square.js
+++ demos/d41classes/square.js
@@ -2,14 +2,25 @@
 /*eslint-disable */
 
 function area() { return this.side * this.side }
-const square1 =  //IMPLEMENTATION NEEDED
+const square1 =  //OBJECT LITERAL
+{
+    side: 5
+};
+square1.__proto__.area = area;
 
-function Square(//IMPLEMENTATION NEEDED
-){}
+function Square(side) { //CONSTRUCTOR FUNCTION
+    this.side = side;
+ }
+ Square.prototype.area = area;
 
 
 
-class Square2 {}//IMPLEMENTATION NEEDED{
+class Square2 { //CLASS SYNTAX
+    constructor(side){
+        this.side = side;
+    }
+    area() { return this.side * this.side };
+ }
 
 
 console.log("expect 25: ", square1.area());
@@ -18,13 +29,21 @@ console.log("expect 1: ", Object.getOwnPropertyNames(square1).length);
 const constSquare1 = new Square(7);
 console.log("expect 49: ", constSquare1.area());
 console.log("expect 1: ", Object.getOwnPropertyNames(constSquare1).length);
+console.log("is it undefined? :", Square.prototype.constructor)
 
 const classSquare = new Square2(10);
 console.log("expect 100: ", classSquare.area());
 console.log("expect 1: ", Object.getOwnPropertyNames(classSquare).length);
 
+let forinArr = [];
+for (let property in classSquare){
+    forinArr.push(property);
+} 
+console.log("for in returns: ", forinArr);
 
-/* Create two morphableSquare objects and draw the object diagram for all the objects involved 
+
+
+/* Create two morphableSquare objects and draw the object diagram for all the objects involved
 square objects
 constructor function
 prototype objects
