8367964ce2a97484e7414025a286e1b573bf65b6
KeithLevi <guilddrive@gmail.com>
2022-09-27 09:57:52 -0500

    preps

--- demos/solutions/clock.js
+++ demos/solutions/clock.js
@@ -7,24 +7,25 @@ function Clock({ template }) {
 }
 
 Clock.prototype.render = function () {
-    let date = new Date();
+        let date = new Date();
 
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
 
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
 
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
 
-    let output = this.template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
 
-    console.log(output);
-}
+        console.log(output);
+    }
+
 
 Clock.prototype.stop = function () {
     clearInterval(this.timer);
