fad7faa225addbf2a85b1d904b0b617e1e0fa37a
KeithLevi <guilddrive@gmail.com>
2023-03-01 18:58:50 -0600

    few updates to d13 and d14 demos

--- demos/d14arrays/sumMatrix.js
+++ demos/d14arrays/sumMatrix.js
@@ -2,7 +2,7 @@
 
 /* Write a function that accepts a two-dimensional array of numbers and returns the sum of all the elements in the array.
 */
-let matrix = [ [1,2,3], [4,5,6], [7,8,9]];
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
 console.log(matrix);
 
@@ -11,10 +11,9 @@ console.log(matrix);
  * @param {Array} matrix is 2-d array
  * @returns {number} total of all elements
  */
-function sumElements(matrix){
-
+function sumElements(matrix) {
 
 }
-    
+
+
 console.log("expect 45: ", sumElements(matrix));
-    
---
+++ demos/d14arrays/tostring.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-irregular-whitespace */
+
+let arr = [1, 2, 3];
+console.log( arr ); // [1,2,3]
+console.log( arr.toString() === "1,2,3" ); // true
+console.log( arr.toString() );
