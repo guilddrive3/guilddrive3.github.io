b69fd57c97d8bcf4b39fbc75d90de96d77ec027e
KeithLevi <guilddrive@gmail.com>
2023-03-03 12:04:12 -0600

    class demos

---
+++ d15fnExps/ex1.js
@@ -0,0 +1,19 @@
+/* eslint-disable require-jsdoc */
+/*Write a function, myCallback( func, arg). myCallback will call func with the given arg and then log the return value to the console.
+Test myCallback by calling it with a function that takes a number and returns the cube of the argument.  First write it as "cube" a normal named function declaration, then as an anonymous function expression.
+Also test with a function that returns true if the arg is an even number
+*/
+
+function myCallback(func, arg){
+
+    //const ret = func(arg);
+    console.log(func(arg));
+
+}
+
+function cube(num){ return num * num * num;}
+function isEven(num) { return num % 2 === 0;}
+
+myCallback(cube, 10); // 1000
+
+myCallback(isEven, 10); // true 
---
+++ d15fnExps/isok.js
@@ -0,0 +1,23 @@
+/* eslint-disable require-jsdoc */
+
+/**
+ * 
+ * @param {*} question is any string
+ * @param {*} yes is fun
+ * @param {*} no  is fun
+ * @return {undefined}
+ */
+function ask(question, yes, no) {
+    if (question) {
+        yes();
+    } else {
+        no();
+  }
+  function showOk() {
+   console.log( "You agreed." );
+  }
+  function showCancel() {
+    console.log( "You canceled the execution." );
+  }
+  ask(0, showOk, showCancel);
+  
