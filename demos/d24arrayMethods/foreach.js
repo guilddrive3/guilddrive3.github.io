7f296bac64111015bbe91551ef6d69e0fe6feb2f
KeithLevi <guilddrive@gmail.com>
2023-03-09 15:10:15 -0600

    d24 class demos

--- demos/d24arrayMethods/foreach.js
+++ demos/d24arrayMethods/foreach.js
@@ -1,4 +1,4 @@
-"use strict";
+
 /* eslint-disable */
 
 //const arr = [1,5,16,3, 108];
@@ -8,30 +8,42 @@
 // function logEvens(num) { if (num % 2 == 0) console.log(num); }
 
 const newArr = [];
-[1, 5, 16, 3, 108].forEach(logEvens2);
+// [1, 5, 16, 3, 108].forEach(logEvens2);
+// [1, 5, 16, 3, 108].forEach(logEvens2);
+const evensArr = [1, 5, 16, 3, 108].filter(function(number) {if (number%2 ===0) {return true} else {return false} });
+console.log("evensArr is: ", evensArr);
 
-function logEvens2(num) { 
-    if (num % 2 == 0) {
-        newArr.push(num); 
-    }
-    //return newArr;
-}
-console.log("newArr is: ", newArr);
+// function logEvens2(num) { 
+//     if (num % 2 == 0) {
+//         newArr.push(num); 
+//     }
+//     //return newArr;
+// }
+// console.log("newArr is: ", newArr);
 
-function isEven(num) {
-    if (num % 2 == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+// function logEvens(arr){
+//     arr.forEach(logEvens2);
+// }
 
-const numbers = [1, 5, 18, 2, 77, 108];
-/* use filter
-all the even numbers */
 
-const evens = numbers.filter(isEven);
-console.log("evens is: ", evens);
+// [1,5,16,3, 108].forEach((num)=> (num%2===0)? console.log(num): 0);
+// [1,5,16,3, 108].forEach((num)=> {if (num%2===0){console.log(num)}; });
 
-console.log("evens is: ", numbers.filter(num => num % 2 === 0));
+
+// function isEven(num) {
+//     if (num % 2 == 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// const numbers = [1, 5, 18, 2, 77, 108];
+// /* use filter
+// all the even numbers */
+
+// const evens = numbers.filter(isEven);
+// console.log("evens is: ", evens);
+
+// console.log("evens is: ", numbers.filter(num => num % 2 === 0));
---
+++ demos/students/d24arrayMethods/methods.js
@@ -0,0 +1,19 @@
+
+export function filterRange(arr, a, b) {
+}
+
+
+export function filterRangeInPlace(arr, a, b) {
+
+}
+
+
+export function Calculator() {
+}
+
+export function unique(arr) {
+}
+
+
+export function groupById(array) {
+}
