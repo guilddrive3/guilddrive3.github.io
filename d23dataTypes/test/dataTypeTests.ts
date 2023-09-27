c1f514daa12bd067b2d962c0e074dc39c2510061
KeithLevi <guilddrive@gmail.com>
2023-09-26 21:01:09 -0500

    d23 updates

--- d23dataTypes/test/dataTypeTests.ts
+++ d23dataTypes/test/dataTypeTests.ts
@@ -1,5 +1,5 @@
-/* comment out the import assert line when running in the browser */
-import assert from "node:assert/strict"; 
+/* comment out the import assert line (in /dist/test js mocha file) when running in the browser */
+import { assert } from "chai";
 
 import { ucFirst, getMaxSubSum, truncate , camelize, checkSpam, extractCurrencyValue} from "../src/app.js";
 
