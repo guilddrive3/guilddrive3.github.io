ebd0b5c6628bb0881de3108903f9207a93dfcbb9
KeithLevi <guilddrive@gmail.com>
2023-10-01 15:06:59 -0500

    final d23 updates

--- d23dataTypes/test/dataTypeTests.ts
+++ d23dataTypes/test/dataTypeTests.ts
@@ -4,7 +4,6 @@ import { assert } from "chai";
 import { ucFirst, getMaxSubSum, truncate , camelize, checkSpam, extractCurrencyValue} from "../src/app.js";
 
  
-/* eslint-disable quotes */
 describe("ucFirst", function() {
   it('Uppercases the first symbol', function() {
     assert.strictEqual(ucFirst("john"), "John");
@@ -29,85 +28,85 @@ describe("checkSpam", function() {
   });
 });
 
-/* eslint-enable quotes */
-// describe("truncate", function() {
-//   it("truncate the long string to the given length (including the ellipsis)", function() {
-//     assert.equal(
-//       truncate("What I'd like to tell on this topic is:", 20),
-//       "What I'd like to te…"
-//     );
-//   });
 
-//   it("doesn't change short strings", function() {
-//     assert.equal(
-//       truncate("Hi everyone!", 20),
-//       "Hi everyone!"
-//     );
-//   });
-// });
+describe("truncate", function() {
+  it("truncate the long string to the given length (including the ellipsis)", function() {
+    assert.equal(
+      truncate("What I'd like to tell on this topic is:", 20),
+      "What I'd like to te…"
+    );
+  });
 
-// describe("extractCurrencyValue", function() {
+  it("doesn't change short strings", function() {
+    assert.equal(
+      truncate("Hi everyone!", 20),
+      "Hi everyone!"
+    );
+  });
+});
 
-//   it("for the string $120 returns the number 120", function() {
-//     assert.strictEqual(extractCurrencyValue('$120'), 120);
-//   });
+describe("extractCurrencyValue", function() {
+
+  it("for the string $120 returns the number 120", function() {
+    assert.strictEqual(extractCurrencyValue('$120'), 120);
+  });
 
 
-// });
-// describe("getMaxSubSum", function() {
-//     it("maximal subsum of [1, 2, 3] equals 6", function() {
-//       assert.equal(getMaxSubSum([1, 2, 3]), 6);
-//     });
+});
+describe("getMaxSubSum", function() {
+    it("maximal subsum of [1, 2, 3] equals 6", function() {
+      assert.equal(getMaxSubSum([1, 2, 3]), 6);
+    });
   
-//     it("maximal subsum of [-1, 2, 3, -9] equals 5", function() {
-//       assert.equal(getMaxSubSum([-1, 2, 3, -9]), 5);
-//     });
+    it("maximal subsum of [-1, 2, 3, -9] equals 5", function() {
+      assert.equal(getMaxSubSum([-1, 2, 3, -9]), 5);
+    });
   
-//     it("maximal subsum of [-1, 2, 3, -9, 11] equals 11", function() {
-//       assert.equal(getMaxSubSum([-1, 2, 3, -9, 11]), 11);
-//     });
+    it("maximal subsum of [-1, 2, 3, -9, 11] equals 11", function() {
+      assert.equal(getMaxSubSum([-1, 2, 3, -9, 11]), 11);
+    });
   
-//     it("maximal subsum of [-2, -1, 1, 2] equals 3", function() {
-//       assert.equal(getMaxSubSum([-2, -1, 1, 2]), 3);
-//     });
+    it("maximal subsum of [-2, -1, 1, 2] equals 3", function() {
+      assert.equal(getMaxSubSum([-2, -1, 1, 2]), 3);
+    });
   
-//     it("maximal subsum of [100, -9, 2, -3, 5] equals 100", function() {
-//       assert.equal(getMaxSubSum([100, -9, 2, -3, 5]), 100);
-//     });
+    it("maximal subsum of [100, -9, 2, -3, 5] equals 100", function() {
+      assert.equal(getMaxSubSum([100, -9, 2, -3, 5]), 100);
+    });
   
-//     it("maximal subsum of [] equals 0", function() {
-//       assert.equal(getMaxSubSum([]), 0);
-//     });
+    it("maximal subsum of [] equals 0", function() {
+      assert.equal(getMaxSubSum([]), 0);
+    });
   
-//     it("maximal subsum of [-1] equals 0", function() {
-//       assert.equal(getMaxSubSum([-1]), 0);
-//     });
+    it("maximal subsum of [-1] equals 0", function() {
+      assert.equal(getMaxSubSum([-1]), 0);
+    });
   
-//     it("maximal subsum of [-1, -2] equals 0", function() {
-//       assert.equal(getMaxSubSum([-1, -2]), 0);
-//     });
+    it("maximal subsum of [-1, -2] equals 0", function() {
+      assert.equal(getMaxSubSum([-1, -2]), 0);
+    });
   
-//     it("maximal subsum of [2, -8, 5, -1, 2, -3, 2] equals 6", function() {
-//       assert.equal(getMaxSubSum([2, -8, 5, -1, 2, -3, 2]), 6);
-//     });
-//   });
+    it("maximal subsum of [2, -8, 5, -1, 2, -3, 2] equals 6", function() {
+      assert.equal(getMaxSubSum([2, -8, 5, -1, 2, -3, 2]), 6);
+    });
+  });
 
-// describe("camelize", function() {
+describe("camelize", function() {
 
-//     it("leaves an empty line as is", function() {
-//       assert.equal(camelize(""), "");
-//     });
+    it("leaves an empty line as is", function() {
+      assert.equal(camelize(""), "");
+    });
   
-//     it("turns background-color into backgroundColor", function() {
-//       assert.equal(camelize("background-color"), "backgroundColor");
-//     });
+    it("turns background-color into backgroundColor", function() {
+      assert.equal(camelize("background-color"), "backgroundColor");
+    });
   
-//     it("turns list-style-image into listStyleImage", function() {
-//       assert.equal(camelize("list-style-image"), "listStyleImage");
-//     });
+    it("turns list-style-image into listStyleImage", function() {
+      assert.equal(camelize("list-style-image"), "listStyleImage");
+    });
   
-//     it("turns -webkit-transition into WebkitTransition", function() {
-//       assert.equal(camelize("-webkit-transition"), "WebkitTransition");
-//     });
+    it("turns -webkit-transition into WebkitTransition", function() {
+      assert.equal(camelize("-webkit-transition"), "WebkitTransition");
+    });
   
-//   });
+  });
