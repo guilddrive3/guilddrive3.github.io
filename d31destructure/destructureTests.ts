fe62e741eb45d73680860b76e39318e312cbab19
KeithLevi <guilddrive@gmail.com>
2023-10-20 16:50:55 -0500

    work on d31

--- d31destructure/destructureTests.ts
+++ d31destructure/destructureTests.ts
@@ -1,7 +1,7 @@
-/* comment out the import assert line when running in the browser */
-import assert from "node:assert/strict"; 
+/* comment out the import assert line (in /dist/test js mocha file) when running in the browser */
+import { assert } from "chai";
 
-import { topSalary } from "./destructure.js";
+import { topSalary } from "../src/destructure.js";
 
  
     
