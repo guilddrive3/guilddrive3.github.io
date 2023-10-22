93cc79d16d140aa3de312a1c13cd88d4ff30244a
KeithLevi <guilddrive@gmail.com>
2023-10-22 00:10:33 -0500

    d31 updates

--- d31destructure/src/destructure.ts
+++ d31destructure/src/destructure.ts
@@ -1,6 +1,22 @@
+/* 
+Create the function topSalary(salaries) that returns the name of the top-paid person.
+    If salaries is empty, it should return null.
+    If there are multiple top-paid persons, return any of them.
+P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+*/
+type SalaryObj = { [key: string]: number };
 
 
-export function topSalary(salaries) {
-
+export function topSalary(salaries: SalaryObj): string {
+    let topPaid = "none";
+    let topSalary = 0;
+    for (let [name, salary] of Object.entries(salaries)) {
+        if (salary > topSalary) {
+            topSalary = salary;
+            topPaid = name;
+        }
+    }
+    return topPaid;
 }
+
 
---
+++ d31destructure/test/destructureTests.ts
@@ -0,0 +1,29 @@
+/* comment out the import assert line (in /dist/test js mocha file) when running in the browser */
+import { assert } from "chai";
+
+import { topSalary } from "../src/destructure.js";
+
+ 
+    
+/* 
+Create the function topSalary(salaries) that returns the name of the top-paid person.
+    If salaries is empty, it should return null.
+    If there are multiple top-paid persons, return any of them.
+P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+*/
+describe("topSalary", function() {
+    it("returns top-paid person", function() {
+      const salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+      };
+  
+      assert.equal( topSalary(salaries), "Pete" );
+    });
+  
+    it("returns none for the empty object", function() {
+        console.log("topsalary: ", topSalary({}) );
+      assert.strictEqual( topSalary({}), "none");
+    });
+  });
