3ef1188bc4a97090892bc11ca710751d4c472313
KeithLevi <guilddrive@gmail.com>
2023-10-23 20:39:03 -0500

    d32 final updates

---
+++ d32closures/src/app.ts
@@ -0,0 +1,8 @@
+//write any code you want to test here and run with npm run app
+import {makeArmy } from './closures.js';
+
+const army = makeArmy();
+
+ army[0](); // the shooter number 0 shows 10
+  army[5](); // and number 5 also outputs 10...
+
--- d32closures/src/closures.ts
+++ d32closures/src/closures.ts
@@ -6,11 +6,7 @@
  * @returns {Function} returns a function that tests whether its argument is in the arr
  * 
  */
-export function inArray(arr) {
 
-
-
-}
 
 /**
  * 
@@ -19,29 +15,17 @@ export function inArray(arr) {
  * @returns {Function} returns a function that tests whether its argument is inside the range
  * 
  */
-export function inBetween(low, high) {
 
 
-}
 
-/**
- * 
- * @param {String} fieldName is a property on object to be sorted
- * @returns {Function} sorting function
- */
-export function byField(fieldName){
-
-}
 
 
 /**
  * @returns {Function} closure that returns it's number
  */
-export function makeArmy() {
 
-  }
 
-  let army = makeArmy();
+
   
   //army[0](); // the shooter number 0 shows 10
   //army[5](); // and number 5 also outputs 10...
