ae712446e8dacd610a23392e7d99a53177b7437b
KeithLevi <guilddrive@gmail.com>
2023-10-26 10:21:24 -0500

    binary tree recursion added

--- d34recursion/src/recursion.ts
+++ d34recursion/src/recursion.ts
@@ -1,5 +1,5 @@
 
-export   { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop}
+export   { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop, TreeNode, sumTreeValues}
 
 function sumTo(n: number): number {
     if (n == 1) return 1;
@@ -9,4 +9,11 @@ function sumTo(n: number): number {
 function factorial(n: number): number {
 
     return n;
+}
+
+
+type TreeNode = {
+    value: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
 }
--- d34recursion/test/recursionTests.ts
+++ d34recursion/test/recursionTests.ts
@@ -1,29 +1,20 @@
 /* comment out the import assert line (in /dist/test js mocha file) when running in the browser */
-import { assert } from "chai";
-
- import { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop} from "../src/recursion.js"
-
-type ListNode = {
-    value: number;
-    next: ListNode | null;
-  }
-  let list: ListNode;
- list = {
+//import { assert } from "chai";
+import { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop, sumTreeValues } from "../src/recursion.js";
+let list;
+list = {
     value: 1,
     next: {
-      value: 2,
-      next: {
-        value: 3,
+        value: 2,
         next: {
-          value: 4,
-          next: null
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
         }
-      }
     }
-  };
-
-
-
+};
 describe("recursion tests", function () {
     it("tests summing by recursion", function () {
         assert.strictEqual(sumTo(5), 15);
@@ -33,7 +24,6 @@ describe("recursion tests", function () {
         assert.strictEqual(factorial(5), 120);
         assert.strictEqual(factorial(10), 3628800);
     });
-
     it("tests fibonacci 10", function () {
         assert.strictEqual(fibonacci(10), 55);
     });
@@ -45,14 +35,22 @@ describe("recursion tests", function () {
     });
     it("iterative version prints 1 2 3 4 to console", function () {
         assert.strictEqual(outputListLoop(list), "1 2 3 4 printed to console");
-    }); 
-
+    });
     it("recursive version prints 4 3 2 1 to console", function () {
         assert.strictEqual(reverseList(list), "4 3 2 1 printed to console");
-    });    
+    });
     it("loop version prints 4 3 2 1 to console", function () {
         assert.strictEqual(reverseListLoop(list), "4 3 2 1 printed to console");
-    });    
-
+    });
 });
 
+describe("binary tree recursion", function () {
+    const tn3 = { value: 3, left: null, right: null };
+    const tn5 = { value: 5, left: null, right: null };
+    const tn4 = { value: 4, left: null, right: null };
+    const tn2 = { value: 2, left: tn4, right: tn5 };
+    const binaryTree = { value: 1, left: tn2, right: tn3 };
+    it("tests sum values of binary tree", function () {
+        assert.strictEqual(sumTreeValues(binaryTree), 15);
+    });
+});
