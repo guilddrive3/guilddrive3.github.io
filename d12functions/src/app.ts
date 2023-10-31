e37a866f1d860087b63221056baea13c1f98c4c1
KeithLevi <guilddrive@gmail.com>
2023-10-31 15:19:19 -0500

    d12 class demos

---
+++ d12functions/src/app.ts
@@ -0,0 +1,17 @@
+import { compound } from "./compound.js"
+
+console.log("compound returns 0: ", compound(100,100));
+
+
+function add(a: number, b: number): number {  
+    return a + b;
+  }
+  const sum1: number = add(10,15);
+  
+  const sum2: number = add(9, 5);
+  
+  console.log("sum1 is: ", sum1, "  sum2 is: ", sum2);
+
+
+
+  
