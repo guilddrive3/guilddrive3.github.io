ab61585164a16b55094070e6535261cb4f2795c2
KeithLevi <guilddrive@gmail.com>
2023-11-16 18:42:37 -0600

    class demos

--- d33closures/src/app.ts
+++ d33closures/src/app.ts
@@ -6,3 +6,13 @@
 //  army[0](); // the shooter number 0 shows 10
 //   army[5](); // and number 5 also outputs 10...
 
+let count = 0;
+function foo(inc: number): string { 
+    count += inc;
+    count += 1;
+    let countWarn = "" + count;
+    if (inc > 1) {
+        countWarn = "WARNING:  Increment is greater than 1: " + count;
+
+    }
+    return countWarn; };
