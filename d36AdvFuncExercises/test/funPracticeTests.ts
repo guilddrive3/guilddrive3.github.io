6b4e7de67082984238c0acffdd94f5159882e928
guilddrive3 <guilddrive3@gmail.com>
2023-11-17 21:15:37 -0600

    d36 to ts config

--- d36AdvFuncExercises/test/funPracticeTests.ts
+++ d36AdvFuncExercises/test/funPracticeTests.ts
@@ -1,7 +1,7 @@
-/* comment out the import assert line when running in the browser */
-import assert from "node:assert/strict"; 
+/* comment out the import assert line (in /dist/test js mocha file) when running in the browser */
+import { assert } from "chai";
 
-import { myMap, myFilter, myReduce } from "./funTests.js";
+import { myMap, myFilter, myReduce } from "../src/funTests.js";
 
 
 describe("map filter reduce", function () {
